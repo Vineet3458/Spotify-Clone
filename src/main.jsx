01c4da6fd4cfa37586735ad662777d523126e317
Vineet3458 <vineetyadav3458@gmail.com>
2025-11-27 01:02:29 +0530

    adding features

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import PlayerContextProvider from "./context/PlayerContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <PlayerContextProvider>
+      <App />
+    </PlayerContextProvider>
+  </BrowserRouter>
+);
